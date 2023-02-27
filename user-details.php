05692b61ad855e2b1584b768b01a2250a8ae8891
JaxonDriver <31314439+JaxonDriver@users.noreply.github.com>
2023-02-27 11:38:19 +0100

    Add files via upload

---
+++ user-details.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <li><a href="http://localhost:8888/homework-2/login-form.php">Log In</a></li>
+        <li><a href="http://localhost:8888//homework-2/user-add.php">Create New User</a></li>
+        <li><a href="http://localhost:8888/homework-2/user-list.php">All Users</a></li>
+         <li><a href="http://localhost:8888/homework-2/user-details.php">User Details</a></li>
+    </ul>
+</nav> 
+       
+<p> First Name: Kanye <p>
+<p> Middle Name: Omari <p>
+<p> Last Name: West <p>
+<p> Address: 7815 S. South Shore Drive, Chicago, IL <p>
+<p> Phone Number: (808) 908-4269 <p>
+<p> Email Address: louisvuittondon@hotmail.com
---
+++ user-list.php
@@ -0,0 +1,36 @@
+<nav>
+    <ul>
+        <li><a href="http://localhost:8888/homework-2/login-form.php">Log In</a></li>
+        <li><a href="http://localhost:8888//homework-2/user-add.php">Create New User</a></li>
+        <li><a href="http://localhost:8888/homework-2/user-list.php">All Users</a></li>
+         <li><a href="http://localhost:8888/homework-2/user-details.php">User Details</a></li>
+    </ul>
+</nav> 
+<h1>All Users</h1>
+
+<form action='http://localhost:8888/homework-2/user-add.php'>
+<button class="btn">User-Add Page</button>
+</form> 
+
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Louis_Vuitton_Don1</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Silver_Surfer69</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>OJ_da_Juiceman4</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Slick_Rick2</a> </p>	
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Ken_Carson56 </a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Jordan_Carter33 </a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Michael_Jordan23</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Michael_Jackson21</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Michael_Tyson63</a> </p>
+<p> <b> Username:  </p> </b>
+<p> <a href='file:///Applications/MAMP/htdocs/Homework/user-details.html'>Michael_Phelps17</a> </p>
+
+		
